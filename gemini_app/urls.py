e9ec0dd73d04c68ac5ab33d7dcff015bc409eb9b
shravya yadav <yadavshravya383@gmail.com>
2025-04-11 18:24:58 +0530

    UI and response generation done

--- gemini_app/urls.py
+++ gemini_app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.chat_view, name='chat'),
+    path("", views.chat_page, name="chat"),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    # path('chat/', views.chat_view, name='chat'),
+    path('clear/', views.clear_chat, name='clear_chat'),
+    
 ]
